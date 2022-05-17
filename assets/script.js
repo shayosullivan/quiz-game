9e66c5217a34383666ad072a1742f0b67887fd4f
shay <shayosullivan@hotmail.com>
2022-05-16 20:32:52 -0400

    basic html layout and css

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+//DEPENDENCIES ==============
+//startbutton
+var startButton = document.querySelector("#start");
+
+//STATE ======================
+//
+
+//FUNCTIONS ==============
+//start the timer/game
+function startGame() {}
+
+//USER INTERACTIONS ==============
+//clicks start button
+// start game
+//user guesses an answer
+
+//INITALIZATION ===============
+
+let question = {
+  title: "Answer is 1",
+  question: ["1", "2", "3", "4"],
+  correctAnswer: 1,
+};
+
+console.log(question);
